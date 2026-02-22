feb24a54e5b046f1b7f1bbc95251a1b7b7ca7ad1
Shotaro Itahara <itahara-shotaro@ynl.t.u-tokyo.ac.jp>
2026-02-22 13:49:00 +0900

    update pruning angle in teleop

--- aerial_robot_base/scripts/keyboard_command.py
+++ aerial_robot_base/scripts/keyboard_command.py
@@ -9,8 +9,8 @@ from aerial_robot_msgs.msg import FlightNav
 import rosgraph
 from std_msgs.msg import Float64
 
-close_angle = 170 # change the parameter reffering to angle_talker.py
-open_angle  = -160
+close_angle = 150 # change the parameter reffering to angle_talker.py
+open_angle  = -170
 init_angle = -70
 
 msg = """
